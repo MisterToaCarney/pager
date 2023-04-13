d3be3ccbf14ae2b3f3ac30fd2fde8f48c135501e
Toa Carney <toa@toacarney.nz>
2023-04-13 14:10:38 +1200

    Add nogui option

--- pager_monitor.py
+++ pager_monitor.py
@@ -1,7 +1,11 @@
-from multiprocessing import Process, Event
+#!/usr/bin/env python3
 
-def dsp_process(exit_event):
-    from grc import pager
+from multiprocessing import Process, Event
+import argparse
+
+def dsp_process(exit_event, no_gui: bool = False):
+    if no_gui: from grc import pager_nogui as pager
+    else: from grc import pager
     pager.main()
     exit_event.set()
 
@@ -10,9 +14,13 @@ def demod_process(exit_event):
     exit_event.set()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nogui", action="store_true", help="Run without QT user interface")
+    args = parser.parse_args()
+
     exit_event = Event()
 
-    dsp = Process(target=dsp_process, args=(exit_event,), daemon=True)
+    dsp = Process(target=dsp_process, args=(exit_event, args.nogui), daemon=True)
     demod = Process(target=demod_process, args=(exit_event,), daemon=True)
     dsp.start()
     demod.start()
