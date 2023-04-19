6627e1ef7c192b704a1f66fb6e3a3b46ec939345
Toa Carney <toa@toacarney.nz>
2023-04-20 01:17:35 +1200

    Add a queue to buffer the audio stream

--- demod/demod.py
+++ demod/demod.py
@@ -3,9 +3,11 @@ import zmq
 import zmq.asyncio
 import sys
 import typing
+import time
 
 ctx = zmq.asyncio.Context()
 channels = ["ch1", "ch2"]
+pre_buffer_size = 8
 
 async def watch_stdout(proc: asyncio.subprocess.Process, log_file: typing.BinaryIO):
     while not proc.stdout.at_eof():
@@ -15,17 +17,24 @@ async def watch_stdout(proc: asyncio.subprocess.Process, log_file: typing.Binary
         sys.stdout.buffer.flush()
         log_file.flush()
 
-async def stream_stdin(proc: asyncio.subprocess.Process, q: asyncio.Queue):
+async def stream_stdin(proc: asyncio.subprocess.Process, q: asyncio.Queue, buffer_ready: asyncio.Event):
     while proc.returncode is None:
+        await buffer_ready.wait()
         proc.stdin.write(await q.get())
         await proc.stdin.drain()
 
-async def watch_zmq(channel: str, q: asyncio.Queue):
+async def watch_zmq(channel: str, q: asyncio.Queue, buffer_ready: asyncio.Event):
     with ctx.socket(zmq.SUB) as s:
         s.setsockopt(zmq.SUBSCRIBE, b"")
         s.connect(f"ipc:///tmp/pager_{channel}.socket")
         while True:
+            start = time.monotonic()
             msg_bytes, = await s.recv_multipart()
+            end = time.monotonic()
+
+            if end - start > 1: buffer_ready.clear()
+            elif q.qsize() > pre_buffer_size: buffer_ready.set()
+
             await q.put(msg_bytes)
 
 async def run_multimon(label: str = "ch0"):
@@ -47,18 +56,18 @@ async def run():
 
     for channel in channels:
         new_proc = await run_multimon(label=channel)
-        new_q = asyncio.Queue(128)
+        new_q = asyncio.Queue()
+        new_buffer_ready = asyncio.Event()
 
         procs.append(new_proc)
         q.append(new_q)
         
-        zmq_tasks.append(asyncio.create_task(watch_zmq(channel, new_q)))
-        in_tasks.append(asyncio.create_task(stream_stdin(new_proc, new_q)))
+        zmq_tasks.append(asyncio.create_task(watch_zmq(channel, new_q, new_buffer_ready)))
+        in_tasks.append(asyncio.create_task(stream_stdin(new_proc, new_q, new_buffer_ready)))
         out_tasks.append(asyncio.create_task(watch_stdout(new_proc, log_file)))
 
     await asyncio.gather(*[proc.wait() for proc in procs])
 
     log_file.close()
-
 
 asyncio.run(run())
--- demod/zmq_cat.py
+++ demod/zmq_cat.py
@@ -1,11 +1,35 @@
 import zmq
+import zmq.asyncio
 import sys
+import asyncio
+import time
 
-ctx = zmq.Context()
+ctx = zmq.asyncio.Context()
 
-with ctx.socket(zmq.SUB) as s:
-    s.setsockopt(zmq.SUBSCRIBE, b"")
-    s.connect(f"ipc:///tmp/pager_{sys.argv[1]}.socket")
+pre_buffer_size = 8
+
+async def read_socket(q: asyncio.Queue, buffer_ready: asyncio.Event):
+    with ctx.socket(zmq.SUB) as s:
+        s.setsockopt(zmq.SUBSCRIBE, b"")
+        s.connect(f"ipc:///tmp/pager_{sys.argv[1]}.socket")
+        while True:
+            start = time.monotonic()
+            msg_bytes, = await s.recv_multipart()
+            end = time.monotonic()
+
+            if end - start > 1: buffer_ready.clear()
+            elif q.qsize() > pre_buffer_size: buffer_ready.set()
+            
+            await q.put(msg_bytes)
+            
+async def write_stdout(q: asyncio.Queue, buffer_ready: asyncio.Event):
     while True:
-        msg_bytes, = s.recv_multipart()
-        sys.stdout.buffer.write(msg_bytes)
+        await buffer_ready.wait()
+        sys.stdout.buffer.write(await q.get())
+
+async def main():
+    buffer_ready = asyncio.Event()
+    q = asyncio.Queue()
+    await asyncio.gather(read_socket(q, buffer_ready), write_stdout(q, buffer_ready))
+
+if __name__ == "__main__": asyncio.run(main())
