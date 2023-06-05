55aef844e03774a9170e3bec149fa89307864a63
Toa Carney <toa@toacarney.nz>
2023-06-05 20:57:55 +1200

    simplify bufering system

--- demod/zmq_cat.py
+++ demod/zmq_cat.py
@@ -13,18 +13,13 @@ async def read_socket(q: asyncio.Queue, buffer_ready: asyncio.Event):
         s.setsockopt(zmq.SUBSCRIBE, b"")
         s.connect(f"ipc:///tmp/pager_{sys.argv[1]}.socket")
         while True:
-            start = time.monotonic()
             msg_bytes, = await s.recv_multipart()
-            end = time.monotonic()
-
-            if end - start > 1: buffer_ready.clear()
-            elif q.qsize() > pre_buffer_size: buffer_ready.set()
-            
             await q.put(msg_bytes)
+            if q.qsize() > pre_buffer_size: buffer_ready.set()
             
 async def write_stdout(q: asyncio.Queue, buffer_ready: asyncio.Event):
+    await buffer_ready.wait()
     while True:
-        await buffer_ready.wait()
         sys.stdout.buffer.write(await q.get())
 
 async def main():
